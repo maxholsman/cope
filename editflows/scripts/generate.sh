1355e863cefe38f0a7723616d3049d785625f395
Tong Chen <chentong@pbg-h100-1.seas.upenn.edu>
2025-11-05 16:25:24 -0500

    generation

--- editflows/scripts/generate.sh
+++ editflows/scripts/generate.sh
@@ -3,4 +3,6 @@ export CUDA_VISIBLE_DEVICES=1
 python -u generate.py \
 --config /scratch/pranamlab/tong/cope/editflows/configs/config_test.yaml \
 --ckpt /scratch/pranamlab/tong/cope/editflows/outputs/2025.11.05/011146/checkpoint/epoch0090-val29.90.ckpt \
---input 'CC(C)[C@H](NC(=O)[C@H](C)NC(=O)[C@H](C)N)C(=O)N[C@@H](CC(N)=O)C(=O)N[C@H](C(=O)N[C@@H](C)C(=O)O)C(C)C'
+--op_temperature 1 \
+--token_temperature 1 \
+--input 'CC[C@H](C)[C@@H](C(=O)N(C)CC(=O)N(C)[C@@H](CC(C)C)C(=O)N[C@@H](CCSC)C(=O)N(C)[C@H](C(=O)N(C)CC(N)=O)C(C)C)N(C)C(C)=O'
